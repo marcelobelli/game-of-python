c00e8cb6664381eaebc82b8a27bb73fe63b9d780
Marcelo Belli <marcelo@belli.me>
2023-02-09 21:20:54 -0300

    Create Cell

--- game_of_life.py
+++ game_of_life.py
@@ -2,3 +2,9 @@ class Grid:
     def __init__(self, width, height):
         self.width = width
         self.height = height
+
+
+class Cell:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
--- test_game_of_life.py
+++ test_game_of_life.py
@@ -1,7 +1,13 @@
-from game_of_life import Grid
+from game_of_life import Cell, Grid
 
 
 def test_grid_must_have_weight_and_width():
     grid = Grid(10, 10)
     assert grid.width == 10
     assert grid.height == 10
+
+
+def test_cell_must_have_a_position():
+    cell = Cell(1, 1)
+    assert cell.x == 1
+    assert cell.y == 1
